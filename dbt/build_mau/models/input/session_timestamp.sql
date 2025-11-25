31a8aceaa48a4feddc1dcd5dd3cf9b99e0ebb9d8
oliver-olive <65835019+oliver-olive@users.noreply.github.com>
2025-11-25 10:53:29 -0800

    Delete dbt/build_mau directory

--- dbt/build_mau/models/input/session_timestamp.sql
+++
@@ -1,8 +0,0 @@
-WITH CET AS
-(SELECT
-sessionId,
-ts
-FROM {{source('raw', 'session_timestamp')}}
-WHERE sessionId is not NULL
-)
-SELECT * FROM CET
--- dbt/build_mau/models/output/session_summary.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-u.userId,
-u.sessionId,
-u.channel,
-st.ts
-FROM {{ref("user_session_channel")}} u
-JOIN {{ref("session_timestamp")}} st ON u.sessionId = st.sessionId
